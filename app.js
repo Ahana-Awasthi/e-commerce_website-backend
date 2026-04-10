4f66041f73df4591e277892fdff88a705791bde0
Ahana Awasthi <ahanaawasthi2802@gmail.com>
2026-04-10 21:59:37 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -17,15 +17,27 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors()); // Allow all origins (for testing)
-
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://e-commercewebsite-git-main-ahana-awasthis-projects.vercel.app",
+      "https://e-commerce-website-backend-d84m.onrender.com",
+    ],
+    credentials: true,
+  }),
+);
 // --- Serve Images ---
 app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 // --- MongoDB Connection ---
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB connected successfully"))
+  .then(() => {
+    console.log("✅ MongoDB connected successfully");
+    checkProductCategories(); // move here
+  })
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
 // --- API Routes (users/auth/wishlist) ---
@@ -98,5 +110,4 @@ async function checkProductCategories() {
 }
 
 // Run check after MongoDB connection
-checkProductCategories();
 module.exports = app;
